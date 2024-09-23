c2982120f938ed3296529cdd6426b854194ee550
Jackson Rolando <jprolando7762@gmail.com>
2024-09-23 16:17:58 -0400

    upload some stuff

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -10,12 +10,18 @@ const SearchBox = ({ setSearchResults }) => {
         setSearchResults(searchResults)
     }
 
-    const onBlurTextBox = () => {
+    const clearResults = () => {
         runSearch(null)
     }
 
     const clearText = () => {
         document.getElementById('searchBox').value = ''
+        clearResults()
+    }
+
+    const selectResult = (result) => {
+        clearResults()
+
     }
 
     return (
@@ -25,7 +31,7 @@ const SearchBox = ({ setSearchResults }) => {
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                 </svg>
             </div>
-            <input id='searchBox' autoComplete="off" onBlur={onBlurTextBox} type="text" onChange={runSearch} className="text-cent pt-2.5 pb-1.5 px-2 mr-3 bg-gray-200 w-full focus:outline-none focus:ring-transparent" name="search" placeholder="search for a video" />
+            <input id='searchBox' autoComplete="off" type="text" onChange={runSearch} className="text-cent pt-2.5 pb-1.5 px-2 mr-3 bg-gray-200 w-full focus:outline-none focus:ring-transparent" name="search" placeholder="search for a video" />
             <div onClick={clearText} className="my-2 mx-2 px-1.5 py-1.5 rounded-md hover:bg-gray-400">
                 <svg class="w-3 h-3 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -9,7 +9,10 @@ results will come in the form of:
     'imgUrl': "https://i.ytimg.com/vi/pYu2..."
 }
 */
-const SearchResults = ({ results }) => {
+
+
+
+const SearchResults = ({ results, selectResult }) => {
 
     if (results === null) {
         return (
@@ -18,19 +21,16 @@ const SearchResults = ({ results }) => {
         )
     }
 
-    const resultDivs = []
-
-    results.forEach(result => {
-        resultDivs.push(
-            <div id={result.vid} className="flex p-1 align-middle">
-                <img className="h-14 w-auto" src={result.imgUrl} />
-                <div className="py-1.5 px-2 max-w-full text-nowrap overflow-hidden">
-                    <div className="font-bold">{result.name}</div>
-                    <div className="">{result.channelName}</div>
-                </div>
+    const resultDivs = results.map(result => (
+        <div tabIndex={0} id={result.vid} key={result.vid} className="flex p-1 align-middle" >
+            <img className="h-14 w-auto" src={result.imgUrl} />
+            <div className="py-1.5 px-2 max-w-full text-nowrap overflow-hidden">
+                <div className="font-bold">{result.name}</div>
+                <div className="">{result.channelName}</div>
             </div>
-        )
-    });
+        </div >
+    ))
+
 
     return (
         <div id="results" className="absolute flex flex-col mt-3 w-5/6 rounded-lg shadow-md">
