4f38cd51f379ef25959d7237d20bd181303b2b32
Jackson Rolando <jprolando7762@gmail.com>
2024-09-28 01:33:29 -0400

    added search button

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { React, useState } from "react"
 import SearchResults from "./SearchResults"
 import SearchBox from "./SearchBox"
 import Menu from "./Menu"
-import { loadVid } from "./utils"
+import { loadVid, search } from "./utils"
 import { submitApiKey } from "./cryptoness"
 
 const App = () => {
@@ -17,6 +17,13 @@ const App = () => {
         }
     }
 
+    const runSearch = async (searchTerm) => {
+        searchTerm = searchTerm || ''
+
+        const searchResults = await search(searchTerm)
+        setSearchResults(searchResults)
+    }
+
     const [searchResults, setSearchResults] = useState(null)
     return (
         <div className="flex flex-col align-middle justify-center">
@@ -28,7 +35,8 @@ const App = () => {
             <div className="player m-auto my-6">
                 <iframe id='player' style={{ width: '100%', height: '200px', overflow: 'hidden', borderRadius: '2rem', boxShadow: '0px 2px 5px gray' }} src="https://www.youtube.com/embed/dZlQLZirG2c" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen;" allowFullScreen></iframe>
             </div>
-            <SearchBox setSearchResults={setSearchResults} />
+            <SearchBox runSearch={runSearch} />
+            <button className="bg-blue-600 hover:bg-blue-800 text-white m-auto w-3/4 max-w-xl px-1 py-2 mt-3 rounded-lg" onClick={() => runSearch(document.getElementById('searchBox').value)}>Search</button>
             <div>
                 <SearchResults results={searchResults} loadVid={loadVid} />
             </div>
--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,14 +1,13 @@
-import React, { useEffect } from "react"
-import { search } from "./utils"
+import React from "react"
 
-const SearchBox = ({ setSearchResults }) => {
+const SearchBox = ({ runSearch }) => {
 
-    const runSearch = async (e) => {
-        let searchTerm = e?.target?.value || ''
+    // const runSearch = async (e) => {
+    //     let searchTerm = e?.target?.value || ''
 
-        const searchResults = await search(searchTerm)
-        setSearchResults(searchResults)
-    }
+    //     const searchResults = await search(searchTerm)
+    //     setSearchResults(searchResults)
+    // }
 
     const clearResults = () => {
         runSearch(null)
@@ -19,10 +18,6 @@ const SearchBox = ({ setSearchResults }) => {
         clearResults()
     }
 
-    const selectResult = (result) => {
-        clearResults()
-    }
-
     return (
         <div className="search m-auto w-3/4 max-w-xl bg-gray-200 rounded-lg flex">
             <div className="ps-2.5 pt-2.5">
@@ -30,7 +25,7 @@ const SearchBox = ({ setSearchResults }) => {
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                 </svg>
             </div>
-            <input id='searchBox' autoComplete="off" type="text" onKeyDown={e => { if (e.key == 'Enter') runSearch(e) }} className="text-cent pt-2.5 pb-1.5 px-2 mr-3 bg-gray-200 w-full focus:outline-none focus:ring-transparent" name="search" placeholder="paste link or search" />
+            <input id='searchBox' autoComplete="off" type="text" onKeyDown={e => { if (e.key == 'Enter') runSearch(e.target.value) }} className="text-cent pt-2.5 pb-1.5 px-2 mr-3 bg-gray-200 w-full focus:outline-none focus:ring-transparent" name="search" placeholder="paste link or search" />
             <div onClick={clearText} className="my-2 mx-2 px-1.5 py-1.5 rounded-md hover:bg-gray-400">
                 <svg className="w-3 h-3 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
@@ -39,5 +34,6 @@ const SearchBox = ({ setSearchResults }) => {
         </div>
     )
 }
+
 
 export default SearchBox
